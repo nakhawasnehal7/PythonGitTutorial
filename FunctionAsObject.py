86a314bff88841d2faf567d82ca95a7bac5a7d12
nakhawa <nakhawasnehal7@gmail.com>
2026-03-02 22:27:06 -0500

    added Functions added Global Variables added Nested Functions

---
+++ FunctionAsObject.py
@@ -0,0 +1,11 @@
+show=print
+show('Welcome')
+
+take=input
+print(take('Enter Name'))
+
+def fun():
+    print('Welcome to function')
+
+f=fun
+f()
---
+++ GlobalVariable.py
@@ -0,0 +1,12 @@
+y = 10
+
+
+def add(x):
+    #global y
+
+
+    y = 0
+    return x + y
+
+print(add(7))
+print(y)
